52f0082cfef198ef2f0e8fa90a50fc3a2079e0d7
Naman Goyal <goyalnaman497gmail.com>
2026-05-26 23:37:49 +0530

    data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from CNNClassifier import logger
+from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is a log message from main.py")
+STAGE_NAME = 'Data Ingestion Stage'
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
