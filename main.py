fb964050977caaf6ad96c106aabaafbc26ab2d5a
Naman Goyal <goyalnaman497gmail.com>
2026-05-29 17:26:08 +0530

    Base Model

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from CNNClassifier import logger
 from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
+        data_ingestion = DataIngestionTrainingPipeline()
+        data_ingestion.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
+        raise e
+
+
+if __name__ == '__main__':
+    STAGE_NAME = 'Prepare Base Model'
+    try:
+        logger.info("*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        prepare_base_model = PrepareBaseModelTrainingPipeline()
+        prepare_base_model.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
--- src/CNNClassifier/pipeline/stage_01_data_ingestion.py
+++ src/CNNClassifier/pipeline/stage_01_data_ingestion.py
@@ -24,3 +24,4 @@ if __name__ == '__main__':
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
+        raise e
