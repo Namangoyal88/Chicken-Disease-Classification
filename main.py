7f0fd3defba64ed29603d10664c353eeb97f0e45
Naman Goyal <goyalnaman497gmail.com>
2026-06-04 12:22:48 +0530

    evaluation

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from CNNClassifier import logger
 from CNNClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from CNNClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from CNNClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+from CNNClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
 
 STAGE_NAME = 'Data Ingestion Stage'
-
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -15,14 +17,42 @@ if __name__ == '__main__':
         raise e
 
 
+STAGE_NAME = 'Prepare Base Model'
 if __name__ == '__main__':
-    STAGE_NAME = 'Prepare Base Model'
     try:
         logger.info("*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         prepare_base_model = PrepareBaseModelTrainingPipeline()
         prepare_base_model.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = 'Training'
+if __name__ == "__main__":
+    try:
+        logger.info(f"********************")
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        model_trainer = ModelTrainingPipeline()
+        model_trainer.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+
+
+STAGE_NAME = 'Evaluation'
+if __name__ == "__main__":
+    try:
+        logger.info(f"********************")
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
         raise e
